46b4f52da7c05693f7a0c5bdb20520818c6aa6fa
jakob svenningsson <jaksve@kth.se>
2020-03-14 20:13:56 +0100

    Refactored client and server cli.

--- go_ftp/cmd/server_cli/server_cli.go
+++ go_ftp/cmd/server_cli/server_cli.go
@@ -8,19 +8,15 @@ import (
 )
 
 var (
-	root = flag.String("root", "/tmp", "Root directory of FTP server.")
-	port = flag.String("port", "10000", "Control connection port.")
-	ip   = flag.String("ip", "", "Control connection addr.")
+	pRoot = flag.String("root", "/tmp", "Root directory of FTP server.")
+	pPort = flag.String("port", "10000", "Control connection port.")
+	pIP   = flag.String("ip", "", "Control connection addr.")
 )
 
 func main() {
-	root, port, ip := parseCmdArgs()
-	log.Printf("Starting FTP server on port %s with root %s.\n", root, ip+":"+port)
+	flag.Parse()
+	root, port, ip := *pRoot, *pPort, *pIP
+	log.Printf("Starting FTP server on port: %s, with root: %s.\n", ip+":"+port, root)
 	ftpserver := ftp_server.New(root, ip, port)
 	log.Fatal(ftpserver.Start())
-}
-
-func parseCmdArgs() (string, string, string) {
-	flag.Parse()
-	return *root, *port, *ip
 }
